210bb20ddfc55fba11c60a9da5538c9783cb2428
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 20:27:01 -0300

    modificadas varias cosas

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ $(document).ready(function(){
   var resultado = expresioncita.exec(window.location.href);
 
   if(resultado != null){
-    resultado = resultado[1];
+    token = resultado[1];
 
     // ahora se puede listar los archivos...
 
     // eliminar el ícono
-    $('body').html("Tu token es: "+resultado);
+    $('body').html("Tu token es: "+token);
+    // obtener archivos
+    $.post('https://www.googleapis.com/drive/v2/files', {pageToken: token}, function(data) {
+      $('body').html(data);
+    });
   }else{
     location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/m8/feeds&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
   }
