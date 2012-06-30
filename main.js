ec755bdfa6652311daf5d0dcdd10de8c7b20dc24
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 20:19:59 -0300

    añadido jquery

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if(resultado != null){
   // ahora se puede listar los archivos...
 
   // eliminar el ícono
-  
+  $('body').html("Tu token es: "+resultado);
 }else{
   location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/m8/feeds&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
 }
