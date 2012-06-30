52a2d031595b0ca94da9828f0c58ed5ca40de26d
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 20:03:49 -0300

    reparado bug

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var expresioncita = new RegExp('access_token=(.*)&token_type');
-var resultado = expresioncita.exec(window.location.href)[1];
+var resultado = expresioncita.exec(window.location.href);
 
 if(resultado == null){
   alert("No tenés TOKEN! QUÉ HACÉS SIN TOKEN!?!?!?!?!?!?!?");
+}else{
+  resultado = resultado[1];
+  alert("Tu token es: "+resultado);
 }
