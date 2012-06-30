f147c13d9d6351052bd17c60e55502a565ff4543
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 20:17:34 -0300

    añadida redirección automática para javier

--- main.js
+++ main.js
@@ -8,4 +8,6 @@ if(resultado != null){
 
   // eliminar el ícono
   
+}else{
+  location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/m8/feeds&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
 }
