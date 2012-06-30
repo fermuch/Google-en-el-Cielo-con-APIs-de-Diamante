5f946c649495a66de8a658438b47d79ae83c18f0
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 20:22:57 -0300

    añadido onready de jquery

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var expresioncita = new RegExp('access_token=(.*)&token_type');
-var resultado = expresioncita.exec(window.location.href);
+$(document).ready(function(){
+  var expresioncita = new RegExp('access_token=(.*)&token_type');
+  var resultado = expresioncita.exec(window.location.href);
 
-if(resultado != null){
-  resultado = resultado[1];
+  if(resultado != null){
+    resultado = resultado[1];
 
-  // ahora se puede listar los archivos...
+    // ahora se puede listar los archivos...
 
-  // eliminar el ícono
-  $('body').html("Tu token es: "+resultado);
-}else{
-  location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/m8/feeds&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
-}
+    // eliminar el ícono
+    $('body').html("Tu token es: "+resultado);
+  }else{
+    location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/m8/feeds&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
+  }
+});
