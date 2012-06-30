929ad11827aebc3da48c06ac57a19d0c1a30efec
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 20:10:18 -0300

    ahora funciona

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 var expresioncita = new RegExp('access_token=(.*)&token_type');
 var resultado = expresioncita.exec(window.location.href);
 
-if(resultado == null){
-  alert("No tenés TOKEN! QUÉ HACÉS SIN TOKEN!?!?!?!?!?!?!?");
-}else{
+if(resultado != null){
   resultado = resultado[1];
-  alert("Tu token es: "+resultado);
+
+  // ahora se puede listar los archivos...
 }
