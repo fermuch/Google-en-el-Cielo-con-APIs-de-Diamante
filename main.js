a5fdf40764073889f2a49e98e00eb5e48f54b499
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 21:04:00 -0300

    asdasdass

--- main.js
+++ main.js
@@ -14,6 +14,6 @@ $(document).ready(function(){
       $('body').html(data);
     });
   }else{
-    location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/drive.file&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
+    //location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/drive.file&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
   }
 });
