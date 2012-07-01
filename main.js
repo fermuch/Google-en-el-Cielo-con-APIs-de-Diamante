108ffb80e5dd4a9e9562e03e21f81321616712ca
Fernando Mumbach <fermuch@gmail.com>
2012-06-30 21:02:04 -0300

    asdasdas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var expresioncita = new RegExp('code=(.*)');
+  var expresioncita = new RegExp('access_token=(.*)&');
   var resultado = expresioncita.exec(window.location.search);
 
   if(resultado != null){
@@ -10,10 +10,10 @@ $(document).ready(function(){
     // eliminar el ícono
     $('body').html("Tu token es: "+token);
     // obtener archivos
-    $.post('https://www.googleapis.com/drive/v2/files?projection=FULL&pageToken='+token, {pageToken: token}, function(data) {
+    $.post('https://www.googleapis.com/drive/v2/files', {pageToken: token}, function(data) {
       $('body').html(data);
     });
   }else{
-    location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/drive.file&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=code";
+    location.href = "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/drive.file&client_id=773902439453.apps.googleusercontent.com&redirect_uri=http://rchit.com.ar/diamantes/index.html&response_type=token";
   }
 });
